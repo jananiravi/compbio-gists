12898c03bcfd30873eb8caced0fe1c6a24c4baae
Janani Ravi <janani@msu.edu>
2019-04-17 19:04:26 -0400

    add loops for v5, all DBs

--- sequence-analysis/update_blastdb.sh
+++ sequence-analysis/update_blastdb.sh
@@ -1,34 +1,40 @@
 ## Date Created: April 17, 2019
 ## Updated by: Janani Ravi
 ## Purpose: To update to BLASTDB v5 (& likely all BLASTDBs)
+## The BLASTDB (all, v5) files available as of April 17, 2019: 
+## https://github.com/jananiravi/compbio-gists/blob/master/sequence-analysis/blastdb_list.md
 
 ## This chunk below would work ONLY on MSU's HPC
 # Start w/ ssh username@hpcc.msu.edu | for users w/ write access to blastdb or subfolders
 cd /mnt/research/common-data/Bio/blastdb
 module purge
 # Loading Blast+; https://wiki.hpcc.msu.edu/pages/viewpage.action?pageId=11896703
-module load icc/2017.4.196-GCC-6.4.0-2.28  impi/2017.3.196 BLAST+/2.8.1-Python-2.7.14
+module load icc/2017.4.196-GCC-6.4.0-2.28 impi/2017.3.196 BLAST+/2.8.1-Python-2.7.14
 
 ## Checking latest updates for version 5
 # https://ftp.ncbi.nlm.nih.gov/blast/db/v5/blastdbv5.pdf
-echo "\nHere are the BLASTDB v5 files to be downloaded:\n"
+printf "\nHere are the BLASTDB v5 files to be downloaded:\n"
 update_blastdb.pl --blastdb_version 5 --showall
-## These are the ones available as of April 17, 2019
-# nr_v5
-# nt_v5
-# pdb_v5
-# refseq_rna_v5
-# swissprot_v5
-# taxdb
+update_blastdb.pl --blastdb_version 5 --showall > blastdb_v5_update_list.txt
+
 # Downloading the updates for v5
-echo "\nDownloading the BLASTDB v5 databases & decompressing\n"
-update_blastdb.pl --blastdb_version 5 nr_v5 --decompress
-update_blastdb.pl --blastdb_version 5 nt_v5 --decompress
-update_blastdb.pl --blastdb_version 5 pdb_v5 --decompress
-update_blastdb.pl --blastdb_version 5 refseq_rna_v5 --decompress
-update_blastdb.pl --blastdb_version 5 swissprot_v5 --decompress
-update_blastdb.pl --blastdb_version 5 taxdb --decompress
+printf "\nSTARTING the downloads of BLASTDB v5 databases\n"
+while read f; do
+printf "\nDownloading $f...\n"
+# update_blastdb.pl --blastdb_version 5 $f
+update_blastdb.pl --blastdb_version 5 $f --decompress
+done <blastdb_v5_update_list.txt
 
 # What are the latest files?
-echo "\nHere are ALL the latst BLASTDB files!\nEdit your script to download the necessary ones.\n"
+printf "\nHere are ALL the latst BLASTDB files!\n"
 update_blastdb.pl --passive --showall
+update_blastdb.pl --passive --showall > blastdb_update_list.txt
+
+# Downloading and decompressing all BLASTDB files
+printf "\nDownloading ALL the latest BLASTDBs\n"
+while read f; do
+printf "\nDownloading $f\n"
+update_blastdb.pl $f --passive --decompress
+done <blastdb_update_list.txt
+
+printf "And we are done! :)"
